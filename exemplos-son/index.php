ce58d206dc60b7c73f5d88d0752826c90bf3f6e1
Elton Alex Silva <elton.alex@hotmail.com>
2021-09-02 10:01:30 -0300

    formatação do código

--- exemplos-son/index.php
+++ exemplos-son/index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="pt-BR">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,37 +10,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
     <title>Exemplos para Certificação</title>
-  </head>
-  <body>
-  <div class="container">
-  <div class="row">
-    <h1>Exemplos para Certificação</h1>
-    <a href="https://www.youtube.com/playlist?list=PLt3tq0MBSMpkDwdzAdeMt7qLXLZyPkwbU">Série de Vídeos utilizados neste estudo para Certificação PHP</a>
-    <hr />
+</head>
 
-    <nav>
-    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Atividades</button>
-        <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Respostas</button>
-    </div>
-    </nav>
-    <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-            <?php include "atividades.php"; ?>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Exemplos para Certificação</h1>
+            <a href="https://www.youtube.com/playlist?list=PLt3tq0MBSMpkDwdzAdeMt7qLXLZyPkwbU">Série de Vídeos utilizados neste estudo para Certificação PHP</a>
+            <hr />
+
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Atividades</button>
+                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Respostas</button>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <?php include "atividades.php"; ?>
+                </div>
+                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    <?php include "respostas.php"; ?>
+                </div>
+            </div>
         </div>
-        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-            <?php include "respostas.php"; ?>
-        </div>
     </div>
-
-
-
-
-
-    
-   
-      </div>
-</div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -51,5 +46,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
